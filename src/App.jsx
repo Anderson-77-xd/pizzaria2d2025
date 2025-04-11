22a9921dd46ef76930d360cb76f98f090547c65e
Anderson-77-xd <rm95071@estudante.fieb.edu.br>
2025-04-11 10:47:57 +0000

    vercel alterações

--- src/App.jsx
+++ src/App.jsx
@@ -2,39 +2,26 @@ import { useState } from "react"
 
 function App(){
 
- const [nome, setNome] = useState('Ronaldo')
-  
- const Formulario = () => {
+  const [nome, setNome] = useState('Ronaldo')
 
   return (
-    <>
-      <h3>Formulário</h3>
-      
-      <input
-     className="nome"
-     onChange={(e)=>{setNome(e.target.value)}}
-     type="text" />
-
-<button
-     className="botao"
-     
-     onClick={()=>{alert(nome)}}
-  >
-  CLIQUE AQUI
-</button>
-
-    </>
-  )
-}
-
- return(
     <div>
       <h3>Pizzaria 2D</h3>
-    <Formulario />
-    <Formulario />
-    <Formulario />
-    <Formulario />
-      
+
+      <input
+        className="nome"
+        onChange={(e)=>{setNome(e.target.value)}}
+        type="text" />
+
+      <button
+        className="botao"
+        onClick={()=>{
+          alert('O nome digita foi ' + nome)
+        }
+        }
+      >
+        CLIQUE AQUI
+      </button>
 
     </div>
   )
